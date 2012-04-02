818a5d7d3b8822a17cc823f01a7e4eacab4ed552
Phil Palmieri <phil@rightleaf.com>
2012-04-02 17:12:47 +0000

    testing cloud9

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -7,10 +7,10 @@ class Welcome extends CI_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -26,6 +26,11 @@ class Welcome extends CI_Controller {
     public function repeat($inputString)
     {
         return $inputString;
+    }
+
+    public function testing123()
+    {
+        echo "here";
     }
 }
 
